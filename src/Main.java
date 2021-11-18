3e559b4d62e4d089830bc4caf6bbe4bd57adb5d8
Artem-Mv <Stimul113@gmail.com>
2021-11-18 13:25:46 +0700

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int sum_transfer = 500_000_000;
+        int result = balance + sum_transfer;
+        System.out.println(result);
+    }
+}
